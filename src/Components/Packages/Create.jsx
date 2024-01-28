f5f957ed765c11d19df23f3b0607040c4c066cdd
nurhan sayed <nurhansaydd@gmail.com>
2024-01-28 20:32:34 +0200

    v2

--- src/Components/Packages/Create.jsx
+++ src/Components/Packages/Create.jsx
@@ -1,10 +1,29 @@
 import React, { useState } from 'react';
+import { Collapse } from 'antd';
+import {basicInfoPackage} from '../../common/Items';
+import SaveBtn from '../../common/SaveBtn';
+import CancelBtn from '../../common/CancelBtn';
 
 const Create = () => {
 
+  
+
+
   return(
     <div className='col-span-11'>
-             hello from package create
+<h1 className='text-slate-500 text-bold py-4' > Create New Package</h1>
+
+    <div className='flex flex-col gap-y-6 '>
+<Collapse className='w-5/6 text-left self-center' items={basicInfoPackage} defaultActiveKey={['1']} />
+
+<div className='w-5/6 flex flex-col self-center  '>
+   <div className='self-end flex gap-x-3'> 
+<SaveBtn />
+<CancelBtn />
+    </div>
+</div>
+
+</div>
         </div>
 
 
--- src/common/Items.js
+++ src/common/Items.js
@@ -1,3 +1,54 @@
+const LoseFocusName = (e)=>{
+
+  let nameErrorMsg = document.getElementById("nameErrorMsg");
+
+  if(e.target.value == "")
+  {
+
+    nameErrorMsg.style.visibility = 'visible';
+    
+  }
+  else{
+
+    nameErrorMsg.style.visibility = 'hidden';
+      
+  }
+}
+const handleOnChangeName= (e) =>{
+ // setPassword(e.target.value)
+
+ let nameErrorMsg = document.getElementById("nameErrorMsg");
+  if(e.target.value != ""){
+    nameErrorMsg.style.visibility = 'hidden';
+  }
+}
+const LoseFocusDesc = (e)=>{
+
+  let descErrorMsg = document.getElementById("descErrorMsg");
+
+  if(e.target.value == "")
+  {
+
+    descErrorMsg.style.visibility = 'visible';
+    
+  }
+  else{
+
+    descErrorMsg.style.visibility = 'hidden';
+      
+  }
+}
+const handleOnChangeDesc= (e) =>{
+ // setPassword(e.target.value)
+
+ let descErrorMsg = document.getElementById("descErrorMsg");
+  if(e.target.value != ""){
+
+    descErrorMsg.style.visibility = 'hidden';
+
+  }
+}
+
 const basicInfoItems = [
     {
       key: '1',
@@ -39,6 +90,85 @@ const basicInfoItems = [
   ];
 
 
+  const basicInfoPackage = [
+    {
+      key: '1',
+      label: 'Basic Info',
+      children: 
+      <div className='flex flex-wrap w-full gap-x-5 gap-y-5 pb-5'>
+        <div className='flex flex-col w-[49%]' >
+      <label htmlFor="" className=' p-1 text-sm' >Name </label>
+            <input
+            type="text"
+            id="Name"
+            onBlur={LoseFocusName}
+            onChange={handleOnChangeName}
+            placeholder="Please enter name"
+            className="px-4 py-2  border border-gray-600 rounded-lg  focus:outline-none focus:ring-blue-500 focus:border-blue-500"
+            />
+              <p className='  text-[red] text-xs  invisible' id="nameErrorMsg">Name is required</p>
+            </div>
+        <div className='flex flex-col w-[49%]' >
+    <label htmlFor="" className=' p-1 text-sm' >Desc </label>
+        <input
+        type="text"
+        id="Desc"
+        onBlur={LoseFocusDesc}
+        onChange={handleOnChangeDesc}
+        placeholder="Please enter desc"
+        className="px-4 py-2 border border-gray-600 rounded-lg  focus:outline-none focus:ring-blue-500 focus:border-blue-500"
+        />
+            <p className='  text-[red] text-xs invisible ' id="descErrorMsg">Desc is required</p>
+        </div>
+        <div className='flex  w-[49%]' >
+      <label htmlFor="" className=' p-1 mr-2 text-sm' >Duration </label>
+            <input
+            type="number"
+            id="Duration"
+            
+            className="p-1 w-1/5 border border-gray-300 rounded-lg  focus:outline-none focus:ring-blue-500 focus:border-blue-500"
+            />
+             
+            </div>
+            <div className='flex  w-[49%]' >
+      <label htmlFor="" className=' p-1 mr-2 text-sm' >First branch price </label>
+            <input
+            type="number"
+            id="Duration"
+            
+            className="p-1 w-1/5 border border-gray-300 rounded-lg  focus:outline-none focus:ring-blue-500 focus:border-blue-500"
+            />
+             
+            </div>
+            <div className='flex  w-[49%]' >
+      <label htmlFor="" className=' p-1 mr-2 text-sm' >Second branch price </label>
+            <input
+            type="number"
+            id="Duration"
+            
+            className="p-1 w-1/5 border border-gray-300 rounded-lg  focus:outline-none focus:ring-blue-500 focus:border-blue-500"
+            />
+             
+            </div>
+            <div className='flex  w-[49%]' >
+      <label htmlFor="" className=' p-1 mr-2 text-sm' >Extra branch price </label>
+            <input
+            type="number"
+            id="Duration"
+            
+            className="p-1 w-1/5 border border-gray-300 rounded-lg  focus:outline-none focus:ring-blue-500 focus:border-blue-500"
+            />
+             
+        </div>
+        <div className='flex  w-[49%]' >
+        <label for="default-checkbox" class="mr-2 text-sm dark:text-gray-300">Active</label>
+        <input id="default-checkbox" type="checkbox" value="" class="w-4 border-sky-200 rounded dark:border-sky-300 text-blue-600 " />
+    
+   </div>
+</div> ,
+    },
+
+  ];
 
   const ownerAccountItems = [
     {
@@ -84,4 +214,4 @@ const basicInfoItems = [
 
 
 
-  export  {basicInfoItems, ownerAccountItems};
+  export  {basicInfoItems, ownerAccountItems, basicInfoPackage};
